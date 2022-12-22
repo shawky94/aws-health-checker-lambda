a11e1f9b163e57bb91b787b9560de9e9c3939c0a
shawky94 <ahmed.shawky5300@gmail.com>
2022-12-23 01:01:33 +0400

    chore: update index file

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-const healthChecker = require("health-checker-aws");
+const { sendMail } = require("./utils/sendEmail.js");
+const { makeRequest } = require("./utils/makeRequest.js");
+const { SUCCESS, FAILURE } = require("./utils/constants.js");
+const {
+  create: createHealthCheckRecord,
+  get: getHealthCheckRecods,
+} = require("./repository/healthCheck");
 
-exports.handler = healthChecker.handler;
+exports.handler = async (event, context, callback) => {
+  const url = "http://www.facebooasdsadk1.com/";
+  const checkUrl = await isWebsiteUp(url);
+  console.log(checkUrl);
+
+  await createHealthCheckRecord({
+    url,
+    state: checkUrl ? "UP" : "DOWN",
+    region: "us-east-1",
+  });
+
+  if (!checkUrl) {
+    const prevRecords = await getHealthCheckRecods({
+      url: "http://www.facebooasdsadk1.com/",
+      region: "us-east-1",
+    });
+    console.log(prevRecords);
+    return null;
+    //return sendMail(event, callback);
+  }
+};
+
+const isWebsiteUp = async (url) => {
+  const options = new URL(url);
+
+  const response = await makeRequest(options);
+
+  return response === SUCCESS ? true : false;
+};
